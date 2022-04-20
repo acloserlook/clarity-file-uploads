ea0097961dee725148d690f60d0ff0a109758a1b
418 <emmanuel.davidson@thejitu.com>
2022-04-20 14:02:04 +0300

    update files

--- clarity-file-uploads/index.js
+++ clarity-file-uploads/index.js
@@ -1,4 +1,9 @@
-const {uploadFile, uploadedFiles} = require('./routes/routes')
+const {uploadFile, 
+    uploadedFiles, 
+    deleteFiles, 
+    unattachedCalls, 
+    updateFileInfo
+} = require('./routes/routes')
 
 const authChecker = require('./middleware/auth');
 const multipart = require('parse-multipart-data');
@@ -6,7 +11,7 @@ const {AclStorage} = require('@acl/storage')
 
 const Readable = require("stream").Readable;
 
-
+const aclStorage = new AclStorage()
 
 
 
@@ -18,33 +23,76 @@ module.exports = async function (context, req) {
    
     context.log('JavaScript HTTP trigger function processed a request.');
     const auth = authChecker.enforceAuthentication(req, context.res)
-    console.log(auth)
+    // console.log(auth)
 
+    // console.log(req)
 
     const boundary = multipart.getBoundary(req.headers["content-type"]);
     const files = multipart.parse(req.body, boundary);
-    console.log( files[0].data.toString())
-    const fileInfo = JSON.parse(files[0].data.toString())    
-    const buffer = files[1].data
-    var stream = new Readable();
-    stream.push(buffer);
-    stream.push(null);
-    
-    const aclStorage = new AclStorage()
-    const res =  await aclStorage.saveFile({fileInfo, fileStream: stream })
 
-    console.log(res)
+    // console.log(files)
+
+    let res={};
+    let fileInfo={}
+
+    console.log( files.find((file)=>file.name==='api-options').data.toString()  )
+
+    const finfo = files.find((file)=>file.name==='fileInfo')
+    const buff = files.find((file)=>file.filename)
+
+    console.log(buff)
+    console.log(finfo)
+
+    if(finfo){
+        fileInfo = JSON.parse(finfo.data.toString()) 
+        console.log(fileInfo)
+    }
+
+    if( buff){
+        
+        const buffer = buff.data
+        var stream = new Readable();
+        stream.push(buffer);
+        stream.push(null);
+        
+       
+    }
+    
+    
+    
+    
 
     
     // console.log(req)
     let response;
     if(route==='upload'){ 
+        
+        res =  await aclStorage.saveFile({fileInfo, fileStream: stream })
+    
+        console.log(res)
         response = await uploadFile(fileInfo, res)
         console.log(response)
     }
 
     else if(route==='uploaded-files'){ 
         response = await uploadedFiles(req, context.res)
+        console.log(response)
+    }
+    else if(route==='updateFileInfo'){ 
+        stream.length = buff.data.length
+        res =  await aclStorage.uploadRawFile({...fileInfo, fileStream: stream, mimetype: buff.type })
+
+        console.log(res)
+
+        response = await updateFileInfo(req, fileInfo, context.res)
+        console.log(response)
+    }
+    else if(route==='delete-files'){ 
+        response = await deleteFiles(req, context.res)
+        console.log(response)
+    }
+    else if(route==='unattachedCalls'){ 
+        response = await unattachedCalls(req, context.res)
         console.log(response)
     }
 
