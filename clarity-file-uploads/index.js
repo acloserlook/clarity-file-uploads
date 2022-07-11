c8278de8b1f67440935c9653c6489ac8de3195bd
Charlie-mwaniki <mwaniqicharlie@gmail.com>
2022-07-11 13:33:42 +0300

    CLA-1782: Add Sentry logging configuration

--- clarity-file-uploads/index.js
+++ clarity-file-uploads/index.js
@@ -1,4 +1,4 @@
-
+const Sentry = require('@sentry/node');
 const {uploadFile, 
     uploadedFiles, 
     deleteFiles, 
@@ -14,9 +14,15 @@ const Readable = require("stream").Readable;
 
 const aclStorage = new AclStorage()
 
-
-
-
+Sentry.init({
+    dsn: 'https://0efe2b16f45d41f79b8836ee7f3cc10b@o507407.ingest.sentry.io/5622860',
+    integrations: [
+      new Sentry.Integrations.Http({ tracing: true })
+    ],
+    environment: process.env.ENV,
+    autoSessionTracking: true,
+    tracesSampleRate: 0.25
+  });
 
 module.exports = async function (context, req) {
 
@@ -56,34 +62,38 @@ module.exports = async function (context, req) {
     }
     
     let response;
-    if(route==='upload'){ 
-        res =  await aclStorage.saveFile({fileInfo, fileStream: stream })
-        response = await uploadFile(fileInfo, res)
-    }
-    else if(route==='uploaded-files'){ 
-        response = await uploadedFiles(req, context.res)
-        console.log(response)
-
-    }
-    else if(route==='updateFileInfo'){ 
-        // handle updates without binary data
-        if(buff){
-            stream.length = buff.data.length
-            res =  await aclStorage.uploadRawFile({...fileInfo, fileStream: stream, mimetype: buff.type })
+    try {
+        if(route==='upload'){ 
+            res =  await aclStorage.saveFile({fileInfo, fileStream: stream })
+            response = await uploadFile(fileInfo, res)
         }
-        
-        response = await updateFileInfo(req, fileInfo, context.res)
+        else if(route==='uploaded-files'){ 
+            response = await uploadedFiles(req, context.res)
+            console.log(response)
 
+        }
+        else if(route==='updateFileInfo'){ 
+            // handle updates without binary data
+            if(buff){
+                stream.length = buff.data.length
+                res =  await aclStorage.uploadRawFile({...fileInfo, fileStream: stream, mimetype: buff.type })
+            }
+            
+            response = await updateFileInfo(req, fileInfo, context.res)
+
+        }
+        else if(route==='delete-files'){ 
+            response = await deleteFiles(req, context.res)
+
+        }
+        else if(route==='unattachedCalls'){ 
+            response = await unattachedCalls(req, context.res)
+
+        }
+    } catch (error) {
+        context.log.error(error);
+        Sentry.captureException(error);
     }
-    else if(route==='delete-files'){ 
-        response = await deleteFiles(req, context.res)
-
-    }
-    else if(route==='unattachedCalls'){ 
-        response = await unattachedCalls(req, context.res)
-
-    }
-
     context.res = {
         body: response
     };
